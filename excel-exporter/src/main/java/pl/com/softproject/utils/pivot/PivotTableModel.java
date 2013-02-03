0961641702bb12053f61fe17122b7a6203f48548
Adrian Lapierre <adrian@soft-project.pl>
2013-02-03 18:40:56 +0000

    pivot table

---
+++ excel-exporter/src/main/java/pl/com/softproject/utils/pivot/PivotTableModel.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2012-07-16 the original author or authors.
+ */
+package pl.com.softproject.utils.pivot;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Adrian Lapierre <adrian@softproject.com.pl>
+ */
+public interface PivotTableModel {
+
+    void add(String rowKey, String columnKey, Object value);
+
+    Iterator iterator();
+    
+//    String[] getColumnNames();    
+//    int getRowCount();
+//    int getColumnCount();
+//    Class getColumnClass(int columnIndex);
+//    Object getValueAt(int rowIndex, int columnIndex);
+//    void setValueAt(Object aValue, int rowIndex, int columnIndex);
+    
+}
---
+++ excel-exporter/src/main/java/pl/com/softproject/utils/pivot/PivotTableModelImpl.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2012-07-17 the original author or authors.
+ */
+package pl.com.softproject.utils.pivot;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+/**
+ *
+ * @author Adrian Lapierre <adrian@softproject.com.pl>
+ */
+public class PivotTableModelImpl implements Iterable<Map<String, Object>>, PivotTableModel {
+    
+    private Map<String, Map<String, Object>> map = new LinkedHashMap<String, Map<String, Object>>();
+    
+    @Override
+    public void add(String rowKey, String columnKey, Object value) {
+        Map<String, Object> row = map.get(rowKey);
+        if(row == null) {
+            row = new HashMap<String, Object>();
+            row.put(columnKey, value);
+            map.put(rowKey, row);
+        } else {
+            row.put(columnKey, value);
+        }
+    }
+    
+    @Override
+    public Iterator  iterator() {
+        return new RowIterator(this);
+    }
+    
+    public void test() {
+        System.out.println("map: " + map);
+        
+        for(Map.Entry<String, Map<String, Object>> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+//            for(Map.Entry<String, Object> t : entry.getValue().entrySet()) {
+//                System.out.println("");
+//            }
+        }
+    }
+    
+    public static class RowIterator implements Iterator<Map<String, Object>> {
+        
+        private final Set<Entry<String, Map<String, Object>>> entrys;
+        //private final Iterator<Map<String, Object>> it;
+        private PivotTableModelImpl impl;
+        private Entry<String, Map<String, Object>> currentEntry;
+        private final Iterator<Entry<String, Map<String, Object>>> it;
+        
+
+        RowIterator(PivotTableModelImpl impl) {
+            this.impl = impl;
+            //it = impl.map.values().iterator();
+            entrys = impl.map.entrySet();
+            it = entrys.iterator();
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return it.hasNext();
+        }
+
+        @Override
+        public Map<String, Object> next() {
+            currentEntry = it.next();
+            return currentEntry.getValue();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
+        public String rowKey() {
+            return currentEntry.getKey();
+        }
+    }
+    
+}
